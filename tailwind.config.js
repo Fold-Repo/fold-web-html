6c2c69159ef685fc461b988b10d7b5cc2dc1809e
osomhe1 <osomhe.aleogho@gmail.com>
2025-01-16 14:46:55 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0EA5E9",
+        // secondary: "#FFC107",
+        // danger: "#E91E63",
+        // success: "#4CAF50",
+        // info: "#2196F3",
+        // warning: "#FF9800",
+        gray: "#4B5563",
+        dark: "#111827",
+        dark2: "#101928",
+      },
+    },
+  },
+  plugins: [],
+};
