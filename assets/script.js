0e20eca8572494eb5604631387943883193b90e9
osomhe1 <osomhe.aleogho@gmail.com>
2025-01-28 10:15:05 +0100

    implemeting categori page design

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,72 @@ document.getElementById('menuButton').addEventListener('click', () => {
        alert("Please enter a valid email address.");
      }
    });
+
+  //  brand and select
+   document.addEventListener("DOMContentLoaded", () => {
+     const brandMenuButton = document.getElementById("brand-menu-button");
+     const brandMenu = brandMenuButton.nextElementSibling;
+     const colorMenuButton = document.getElementById("color-menu-button");
+     const colorMenu = colorMenuButton.nextElementSibling;
+     const priceMenuButton = document.getElementById("price-menu-button");
+     const priceMenu = priceMenuButton.nextElementSibling;
+     const sortMenuButton = document.getElementById("sort-menu-button");
+     const sortMenu = sortMenuButton.nextElementSibling;
+
+     function toggleMenu(menu, button) {
+       menu.classList.toggle("hidden");
+       const expanded = button.getAttribute("aria-expanded") === "true";
+       button.setAttribute("aria-expanded", !expanded);
+     }
+
+     brandMenuButton.addEventListener("click", (e) => {
+       toggleMenu(brandMenu, brandMenuButton);
+       e.stopPropagation();
+     });
+     colorMenuButton.addEventListener("click", (e) => {
+       toggleMenu(colorMenu, colorMenuButton);
+       e.stopPropagation();
+     });
+     priceMenuButton.addEventListener("click", (e) => {
+       toggleMenu(priceMenu, priceMenuButton);
+       e.stopPropagation();
+     });
+
+     sortMenuButton.addEventListener("click", (e) => {
+       toggleMenu(sortMenu, sortMenuButton);
+       e.stopPropagation();
+     });
+     document.addEventListener("click", (event) => {
+       if (
+         !brandMenuButton.contains(event.target) &&
+         !brandMenu.contains(event.target)
+       ) {
+         brandMenu.classList.add("hidden");
+         brandMenuButton.setAttribute("aria-expanded", false);
+       }
+       if (
+         !colorMenuButton.contains(event.target) &&
+         !colorMenu.contains(event.target)
+       ) {
+         colorMenu.classList.add("hidden");
+         colorMenuButton.setAttribute("aria-expanded", false);
+       }
+       if (
+         !priceMenuButton.contains(event.target) &&
+         !priceMenu.contains(event.target)
+       ) {
+         priceMenu.classList.add("hidden");
+         priceMenuButton.setAttribute("aria-expanded", false);
+       }
+
+       if (
+         !sortMenuButton.contains(event.target) &&
+         !sortMenu.contains(event.target)
+       ) {
+         sortMenu.classList.add("hidden");
+         sortMenuButton.setAttribute("aria-expanded", false);
+       }
+     });
+   });
+
+  
